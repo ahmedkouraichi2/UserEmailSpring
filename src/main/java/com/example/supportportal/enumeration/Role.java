e45d9cb09af91f73038aacb9ef04695df2bcd9ab
ahmedkouraichi2 <64045126+ahmedkouraichi2@users.noreply.github.com>
2023-08-17 13:44:36 +0200

    first commit

---
+++ src/main/java/com/example/supportportal/enumeration/Role.java
@@ -0,0 +1,20 @@
+package com.example.supportportal.enumeration;
+import static com.example.supportportal.constant.Authority.*;
+public enum Role {
+    ROLE_USER(USER_AUTHORITIES),
+    ROLE_HR(HR_AUTHORITIES),
+    ROLE_MANAGER(MANAGER_AUTHORITIES),
+    ROLE_ADMIN(ADMIN_AUTHORITIES),
+    ROLE_SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES);
+
+    private String[] authorities;
+
+    Role(String... authorities) {
+        this.authorities = authorities;
+    }
+
+    public String[] getAuthorities() {
+        return authorities;
+    }
+}
+
